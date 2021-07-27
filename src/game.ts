56afacad97fe5b56486040db572ab8d0529f1828
Pandaphobic <c.steffes@live.ca>
2021-07-27 11:14:41 -0500

    tweak emojis

--- src/game.ts
+++ src/game.ts
@@ -4,26 +4,26 @@ class Board {}
 
 [                 LEGEND                   ]
 🏁 - Go Space             🆓 - Free Parking
-🧰 - Community Chest      🟦 - Blue Property
+💰 - Community Chest      🟦 - Blue Property
 🚂 - Railroad             🟩 - Blue Property
 🤞 - Chance               🟨 - Blue Property
-🏦 - Pay Taxes            🟥 - Blue Property
-🏛️ - Jail                 🟧 - Blue Property
-👮‍♂️ - Goto Jail            🟪 - Blue Property
+🏛️ - Pay Taxes            🟥 - Blue Property
+🏦 - Jail                 🟧 - Blue Property
+🚔 - Goto Jail            🟪 - Blue Property
 💡 - Electric Company     ⬜ - Blue Property
 🚰 - Water                🟫 - Blue Property
 
 
-  [🆓][🟥][🤞][🟥][🟥][🚂][🟨][🟨][🚰][🟨][👮‍♂️]
-  [🟧]                                 [🟩]
-  [🟧]                                 [🟩]
-  [🧰]                                 [🧰]
-  [🟧]                                 [🟩]
-  [🚂]         CLI-Opoly Board         [🚂]
-  [🟪]                                 [🤞]
-  [🟪]                                 [🟦]
-  [💡]                                 [🏦]
-  [🟪]                                 [🟦]
-  [🏛️][⬜][⬜][🤞][⬜][🚂][🏦][🟫][🧰][🟫][🏁]
+  [🆓][🟥][🤞][🟥][🟥][🚂][🟨][🟨][🚰][🟨][🚔] 
+  [🟧]                                     [🟩] 
+  [🟧]                                     [🟩] 
+  [💰]                                     [💰] 
+  [🟧]                                     [🟩] 
+  [🚂]         CLI-Opoly Board             [🚂] 
+  [🟪]                                     [🤞] 
+  [🟪]                                     [🟦] 
+  [💡]                                     [🏦] 
+  [🟪]                                     [🟦] 
+  [🏦][⬜][⬜][🤞][⬜][🚂][🏛️][🟫][💰][🟫][🏁] 
 
 */
