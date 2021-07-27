5e6f30d64ff78784bb1dba3e642e172d5e5e27bf
Chris Steffes <c.steffes@live.ca>
2021-07-27 09:38:21 -0500

    board in emojis

---
+++ src/game.ts
@@ -0,0 +1,29 @@
+class Board {}
+
+/*
+
+[                 LEGEND                   ]
+🏁 - Go Space             🆓 - Free Parking
+🧰 - Community Chest      🟦 - Blue Property
+🚂 - Railroad             🟩 - Blue Property
+🤞 - Chance               🟨 - Blue Property
+🏦 - Pay Taxes            🟥 - Blue Property
+🏛️ - Jail                 🟧 - Blue Property
+👮‍♂️ - Goto Jail            🟪 - Blue Property
+💡 - Electric Company     ⬜ - Blue Property
+🚰 - Water                🟫 - Blue Property
+
+
+  [🆓][🟥][🤞][🟥][🟥][🚂][🟨][🟨][🚰][🟨][👮‍♂️]
+  [🟧]                                 [🟩]
+  [🟧]                                 [🟩]
+  [🧰]                                 [🧰]
+  [🟧]                                 [🟩]
+  [🚂]         CLI-Opoly Board         [🚂]
+  [🟪]                                 [🤞]
+  [🟪]                                 [🟦]
+  [💡]                                 [🏦]
+  [🟪]                                 [🟦]
+  [🏛️][⬜][⬜][🤞][⬜][🚂][🏦][🟫][🧰][🟫][🏁]
+
+*/
