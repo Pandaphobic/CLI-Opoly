cf94f01898da6b732aed4ac259d80f05bb423e8b
Pandaphobic <c.steffes@live.ca>
2021-07-30 10:15:17 -0500

    layout progress

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,7 @@ import { whoGoesFirst, whosTurnIsIt, movePlayer } from "./game";
 var emoji = require("node-emoji");
 import chalk = require("chalk");
 
-var [coffee] = [emoji.get("coffee")];
+var [coffee, blueSquare] = [emoji.get("coffee"), emoji.get("blueSquare")];
 
 const Players: Player[] = [];
 const Board = BuildBoard();
@@ -24,31 +24,75 @@ screen.title = "CLI-Opoly - A CLI Take on the Classic Game!";
 //create layout and widgets
 var grid = new contrib.grid({ rows: 4, cols: 12, screen: screen });
 
-var player_explorer = grid.set(0, 0, 1, 2, contrib.table, {
-  style: {
-    // fg: "red",
-  },
-  selectedBg: "green",
-  template: { lines: true },
-  label: chalk.bold(`Players`),
-  width: "50%",
-  height: "50%",
-  columnSpacing: 10, //in chars
-  columnWidth: [16, 12, 12] /*in chars*/,
-});
-
-var player_details = grid.set(1, 0, 2, 2, contrib.table, {
+var player_details = grid.set(2, 0, 1, 2, contrib.table, {
   keys: true,
   fg: "white",
-  selectedFg: "white",
-  selectedBg: "blue",
+  selectedFg: "black",
+  selectedBg: "cyan",
   interactive: true,
-  label: "Details",
+  label: `${chalk.bold("Details")}`,
   width: "30%",
   height: "30%",
   border: { type: "line", fg: "cyan" },
-  columnSpacing: 10, //in chars
-  columnWidth: [16, 12, 12] /*in chars*/,
+  columnSpacing: 0, //in chars
+  columnWidth: [0, 0, 0] /*in chars*/,
+});
+
+var player_explorer = grid.set(0, 0, 2, 2, contrib.tree, {
+  style: {
+    fg: "white",
+  },
+  selectedBg: "cyan",
+  template: { lines: true },
+  label: `${chalk.bold("Player Explorer")}`,
+  padding: 0,
+});
+
+//set tree
+player_explorer.setData({
+  extended: true,
+  children: {
+    Chris: {
+      children: {
+        Properties: ["baltic", "boardwalk"],
+        Wallet: {
+          Cash: "$500",
+          Loans: "$0",
+          Cards: {},
+        },
+      },
+    },
+    Calista: {
+      children: {
+        Properties: ["baltic", "boardwalk"],
+        Wallet: {
+          Cash: "500",
+          Loans: {},
+          Cards: {},
+        },
+      },
+    },
+    Michael: {
+      children: {
+        Properties: ["baltic", "boardwalk"],
+        Wallet: {
+          Cash: "500",
+          Loans: {},
+          Cards: {},
+        },
+      },
+    },
+    Megan: {
+      children: {
+        Properties: ["baltic", "boardwalk"],
+        Wallet: {
+          Cash: "500",
+          Loans: {},
+          Cards: {},
+        },
+      },
+    },
+  },
 });
 
 var trade = grid.set(3, 0, 1, 3, contrib.table, {
@@ -85,7 +129,7 @@ var turn_prompts = grid.set(3, 6, 1, 3, contrib.table, {
   selectedFg: "white",
   selectedBg: "blue",
   interactive: true,
-  label: "Details",
+  label: "Turn Prompts",
   width: "30%",
   height: "30%",
   border: { type: "line", fg: "cyan" },
@@ -129,8 +173,14 @@ screen.key(["tab"], function () {
 
 function focusedDraw() {
   // if (screen.focused === player_explorer.rows) {
-  //   player_explorer.setLabel(`${chalk.bgMagentaBright("Players")}`);
+  //   player_explorer.setLabel(`${chalk.bgMagentaBright("Soemthing")}`);
+  // } else {
+  //   player_explorer.setLabel(`${chalk.bgWhiteBright.black("Players")}`);
   // }
+  // player_details
+  // trade
+  // buy_sell_houses
+  // chat
 }
 
 player_explorer.focus();
@@ -143,4 +193,7 @@ export function logTurn(message: string) {
 
 // Determine who goes first
 whoGoesFirst(Players);
+movePlayer(whosTurnIsIt(Players), Board, Players);
+movePlayer(whosTurnIsIt(Players), Board, Players);
+movePlayer(whosTurnIsIt(Players), Board, Players);
 movePlayer(whosTurnIsIt(Players), Board, Players);
